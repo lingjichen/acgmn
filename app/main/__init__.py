0380060f3861e18036b52b89c43947787c132258
chenlingji <499910843@qq.com>
2017-09-13 21:55:16 +0800

    role permission

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -7,3 +7,9 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors
+from ..models import Permission
+
+#巴Permission加入模板上下文？有疑问
+@main.app_context_processor()
+def inject_permissions():
+    return dict(Permission=Permission)
