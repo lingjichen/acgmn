b6c640bac7b8808136b4dc21f566371c40f01297
chenlingji <499910843@qq.com>
2017-09-14 12:55:49 +0800

    edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,9 +2,48 @@
 # -*- coding:utf-8 -*
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, \
+    SubmitField
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from ..models import Role, User
 
 class NameForm(FlaskForm):
     name = StringField('你的名字是？', validators=[Required()])
     submit = SubmitField('提交')
+
+class EditProfileForm(FlaskForm):
+    name = StringField('真实姓名', validators=[Length(0, 64)])
+    location = StringField('地址', validators=[Length(0, 64)])
+    about_me = TextAreaField('关于我')
+    submit = SubmitField('提交')
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('邮箱', validators=[Required(), Length(1,64),
+                                          Email()])
+    username = StringField('用户名', validators=[
+        Required(), Length(1,64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                         '用户名只能包含字母，数字，下划线，点号')
+    ])
+    confirmed = BooleanField('确认')
+    role = SelectField('权限', coerce=int)
+    name = StringField('真实姓名', validators=[Length(0,64)])
+    location = StringField('地址', validators=[Length(0, 64)])
+    about_me = TextAreaField('关于我')
+    submit = SubmitField('提交')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.quey.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经被注册了')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经被使用了')
--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+from datetime import datetime
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 
@@ -57,6 +59,12 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -65,6 +73,9 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(
+                self.email.encode('utf-8')).hexdigest()
 
     @property
     def password(self):
@@ -111,7 +122,7 @@ class User(UserMixin, db.Model):
 
     def generate_email_change_token(self, new_email, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'change_email': self.id, 'new_email': nwe_email})
+        return s.dumps({'change_email': self.id, 'new_email': new_email})
 
     def change_email(self, token):
         s =Serializer(current_app.config['SECRET_KEY'])
@@ -124,9 +135,11 @@ class User(UserMixin, db.Model):
         new_email = data.get('nwe_email')
         if new_email is None:
             return False
-        if self.query.filter_by(email=nwe_email).first() is not None:
+        if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
 
@@ -136,6 +149,21 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://secure.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating
+        )
 
     def __repr__(self):
         return '<User %r>' % self.username
